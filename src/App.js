5da90ae57343781d9ccfab2128cd0c65a0382483
Shamil Saleem C <shamilsaleemc@gmail.com>
2024-08-03 23:32:39 +0530

    Delete button and function added

--- src/App.js
+++ src/App.js
@@ -7,42 +7,44 @@ import TodoList from "./components/TodoList";
 export const newToDo = createContext();
 
 export default function App() {
-  const [toDos, changeToDos] = useState(JSON.parse(localStorage.getItem('toDos')) || []);
+  const [toDos, changeToDos] = useState(
+    JSON.parse(localStorage.getItem("toDos")) || []
+  );
 
   useEffect(() => {
-    localStorage.setItem('toDos', JSON.stringify(toDos));
+    localStorage.setItem("toDos", JSON.stringify(toDos));
   }, [toDos]);
 
   function renderTodos() {
     return toDos.map(({ text, time, status }, index) => {
-      var timeObject = new Date(time)
+      var timeObject = new Date(time);
       return <TodoList todo={{ text, timeObject, status, index }} />;
     });
   }
 
   return (
-  <div className="container">
-    <div className="col-12">
-      <div className="row">
-        <h1 className="center mt-3">ToDo List</h1>
-        <p className="center">Simple ToDo app using React</p>
-      </div>
-      <div className="row">
-        <DateAndTime />
-      </div>
-      <div className="row">
-        <div className="d-grid gap-2 col-6 mx-auto">
+    <div className="container">
+      <div className="col-12">
+        <div className="row">
+          <h1 className="center mt-3">ToDo List</h1>
+          <p className="center">Simple ToDo app using React</p>
+        </div>
+        <div className="row">
+          <DateAndTime />
+        </div>
+        <div className="row">
+          <div className="d-grid gap-2 col-6 mx-auto">
+            <newToDo.Provider value={{ toDos, changeToDos }}>
+              <AddToDo />
+            </newToDo.Provider>
+          </div>
+        </div>
+        <div className="row">
           <newToDo.Provider value={{ toDos, changeToDos }}>
-            <AddToDo />
+            <div className="col-sm-12 mb-3 mb-sm-0">{renderTodos()}</div>
           </newToDo.Provider>
         </div>
       </div>
-      <div className="row">
-        <newToDo.Provider value={{ toDos, changeToDos }}>
-          <div className="col-sm-12 mb-3 mb-sm-0">{renderTodos()}</div>
-        </newToDo.Provider>
-      </div>
     </div>
-  </div>
-    );
+  );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -12,14 +12,31 @@ function TodoList(props) {
       return toDo
     }))
   }
+
+  function delToDo(key) {
+    var newToDoList = []
+    newToDoOps.toDos.map((toDo, index) => {
+      if (index !== key) {
+        newToDoList.push(toDo)
+      }
+    })
+    newToDoOps.changeToDos(newToDoList)
+  }
+
   return (
     <div key={props.todo.index} className="card mt-3">
       <div className="card-header">
         #{props.todo.index + 1} · {props.todo.timeObject.getHours()}:
-        {props.todo.timeObject.getHours()>= 10 ? props.todo.timeObject.getMinutes() : "0" + props.todo.timeObject.getMinutes()}
-        <input className="form-check-input float-end"
-          type="checkbox"
-          onChange={() => toDoStatechange(props.todo.index)} />
+        {props.todo.timeObject.getMinutes() >= 10 ? props.todo.timeObject.getMinutes() : "0" + props.todo.timeObject.getMinutes()}
+        <div className="float-end">
+          <i className="fa-solid fa-trash" style={{ marginRight: "4px" }} onClick={() => delToDo(props.todo.index)}></i>
+          {props.todo.status ? <input className="form-check-input"
+            type="checkbox"
+            onChange={() => toDoStatechange(props.todo.index)} /> :
+            <input className="form-check-input"
+              type="checkbox"
+              onChange={() => toDoStatechange(props.todo.index)} checked />}
+        </div>
       </div>
       <div className="card-body">
         {props.todo.status ? <h5 className="card-title">{props.todo.text}</h5> : <h5 className="card-title text-body-secondary"><del>{props.todo.text}</del></h5>}
